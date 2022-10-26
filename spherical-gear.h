ac063703bd072d7a6fd432ff9e85a8beb251ce82
henry-lignux <stevenshenry2017@gmail.com>
2022-10-26 14:28:16 -0700

    changed move function

--- spherical-gear.h
+++ spherical-gear.h
@@ -1,6 +1,6 @@
 #include <Servo.h> // for the motor
 #include <cmath> // for the math
-
+#define TIME_ITERATION 10 // used for servo delay
 class SphericalGear {
   private:
     Servo servo1; // servo that changes angle1
@@ -19,13 +19,13 @@ class SphericalGear {
       double time = total_angle / angular_speed; // in miliseconds
       double theta_speed = theta / time;
       double phi_speed = phi / time;
-      for(int t = 0; t <= time; i+=10) {
-        // iterates over 10 miliseconds
-        servo1.write(angle1);
+      for(int t = 0; t <= time; t+=TIME_ITERATION) { 
+	// iterates over 10 miliseconds 
+	servo1.write(angle1);
         servo2.write(angle2);
-        angle1 += theta_speed*10;
-        angle2 += phi_speed*10;
-        delay(10);
+        angle1 += theta_speed*TIME_ITERATION;
+        angle2 += phi_speed*TIME_ITERATION;
+        delay(TIME_ITERATION);
       }
     }
 };
