9615d99a69f4bf3470d2b0d843f7a2cbc474bda0
henry-lignux <stevenshenry2017@gmail.com>
2022-10-31 12:54:08 -0700

    updated equation in move function

--- spherical-gear.h
+++ spherical-gear.h
@@ -3,10 +3,10 @@
 #define TIME_ITERATION 10 // used for servo delay
 class SphericalGear {
   private:
-    Servo servo1; // servo that changes angle1
-    Servo servo2; // servo that changes angle2
-    double angle1; // theta
-    double angle2; // phi
+    Servo servo1; // servo object for motor that changes theta
+    Servo servo2; // servo object for motor that changes phi
+    double theta;
+    double phi;
 	public:
 		SphericalGear(int pin1, int pin2) {
       servo1.attach(pin1);
@@ -14,17 +14,18 @@ class SphericalGear {
       angle1 = 0;
       angle2 = 0;
 		}
-    void move(double theta, double phi, double angular_speed) {
-      double total_angle = acos(cos(theta) * cos(phi)); // in degrees
-      double time = total_angle / angular_speed; // in miliseconds
-      double theta_speed = theta / time;
-      double phi_speed = phi / time;
+    void move(double delta_theta, double delta_phi, double angular_speed) {
+      // radius is assumed to be 1
+      double total_angle_change = acos( sin(theta)*sin(theta+delta_theta)*cos(phi-delta_phi) + cos(theta)*cos(theta+delta_theta) ); // measured in degrees; from mr. nims's equation
+      double time = total_angle_change / angular_speed; // in miliseconds
+      double theta_speed = delta_theta / time;
+      double phi_speed = delta_phi / time;
       for(int t = 0; t <= time; t+=TIME_ITERATION) { 
 	      // iterates over TIME_ITERATION miliseconds 
-	      servo1.write(angle1);
-        servo2.write(angle2);
-        angle1 += theta_speed*TIME_ITERATION;
-        angle2 += phi_speed*TIME_ITERATION;
+	      servo1.write(theta);
+        servo2.write(phi);
+        theta += theta_speed*TIME_ITERATION;
+        phi += phi_speed*TIME_ITERATION;
         delay(TIME_ITERATION);
       }
     }
