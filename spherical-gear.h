f2a6dfb7e5e590766d93bf02e84fc5c1b38a5f6d
aidsmin <scmin1018@gmail.com>
2022-11-01 08:56:56 -0700

    added descriptive comments

--- spherical-gear.h
+++ spherical-gear.h
@@ -9,21 +9,24 @@ class SphericalGear {
     double phi;
 	public:
 		SphericalGear(int pin1, int pin2) {
+      // initializing the motors
       servo1.attach(pin1);
       servo2.attach(pin2);
-      angle1 = 0;
-      angle2 = 0;
+      // initializing the angles
+      theta = 0;
+      phi = 0;
 		}
     void move(double delta_theta, double delta_phi, double angular_speed) {
       // radius is assumed to be 1
-      double total_angle_change = acos( sin(theta)*sin(theta+delta_theta)*cos(phi-delta_phi) + cos(theta)*cos(theta+delta_theta) ); // measured in degrees; from mr. nims's equation
+      // measured in degrees; from mr. nims's equation
+      double total_angle_change = acos( sin(theta)*sin(theta+delta_theta)*cos(phi-delta_phi) + cos(theta)*cos(theta+delta_theta) );
       double time = total_angle_change / angular_speed; // in miliseconds
       double theta_speed = delta_theta / time;
       double phi_speed = delta_phi / time;
       for(int t = 0; t <= time; t+=TIME_ITERATION) { 
 	      // iterates over TIME_ITERATION miliseconds 
-	      servo1.write(theta);
-        servo2.write(phi);
+	      servo1.write(theta); // moves motor to theta
+        servo2.write(phi); // moves motor to phi
         theta += theta_speed*TIME_ITERATION;
         phi += phi_speed*TIME_ITERATION;
         delay(TIME_ITERATION);
