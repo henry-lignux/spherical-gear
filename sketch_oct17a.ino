6f7c059631e25baf1c2b9da3002f7ff1a5f04748
thelee2873 <brandonlee2873@gmail.com>
2023-01-04 08:32:38 -0700

    added delays

--- sketch_oct17a.ino
+++ sketch_oct17a.ino
@@ -9,8 +9,8 @@ void setup() {
   SphericalGear sg(motor1_pin,motor2_pin);
   Servo claw_servo;
   claw_servo.attach(claw_pin); // initialize motor
-  int angle_openClaw = 30;
-  int angle_closeClaw = 10;
+  int angle_open_claw = 30;
+  int angle_close_claw = 10;
   //we want the arm to reach down, then grab with the claw, then pull back up
   //we might want to go back down and release the claw as well
   
@@ -19,17 +19,21 @@ void setup() {
   // arm starts at vertical 90 degrees, with claw closed
   
   // open claw
-  openclaw(claw_servo, angle_openClaw);
+  openclaw(claw_servo, angle_open_claw);
+  delay(500);
   // move arm down
   move(90,0,30);
   //close claw 
-  closeclaw(claw_servo, angle_closeClaw);
+  closeclaw(claw_servo, angle_close_claw);
   //move arm up
   move(-90,0, 30);
   //move arm down
+  delay(500);
   move(90,0,30);
   //open claw
-  openclaw(claw_servo, angle_openClaw);
+  delay(500);
+  openclaw(claw_servo, angle_open_claw);
+  delay(500);
   //move arm up
   move(-90,0, 30);
 }
